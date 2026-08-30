e34902959bb05f68f6bbd7a24d3b33df85e8c6b8
KigoryGH <alialmowali213@gmail.com>
2026-08-30 10:08:35 +0300

    squarify

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 import chalk from "chalk";
-import { squarify } from "squarify";
-import fs from "fs"
+import fs from "fs";
 import path from "path";
+import squarifyModule from 'squarify';
+const squarify = squarifyModule.default;
 
-const entries = fs.readdirSync(".")
+const entries = fs.readdirSync(".");
 
 function formatSize(bytes) {
     const units = ["B", "KB", "MB", "GB"];
     let size = bytes;
     let unitIndex = 0;
 
-    while (size >= 1024 && unitIndex < units.length - 1) {
-        size /= 1024;
+    while (size >= 1000 && unitIndex < units.length - 1) {
+        size /= 1000;
         unitIndex++;
     }
 
     return size.toFixed(1) + " " + units[unitIndex];
-}
-
-const info=[]
-
-for (const name of entries) {
-const stats = fs.statSync(name);
-    console.log(name, formatSize(getSize(name)),);
 }
 
 function getSize(path) {
@@ -38,5 +32,17 @@ function getSize(path) {
         total += getSize(path + "/" + child);
     }
     return total;
-
 }
+
+const info = [];
+
+for (const fName of entries) {
+    console.log(fName, formatSize(getSize(fName)));
+    info.push({ name: fName, value: getSize(fName) });
+}
+
+console.log(info);
+
+const container = { x0: 0, y0: 0, x1: 40, y1: 15 };
+const layout = squarify(info, container);
+console.log(layout);
